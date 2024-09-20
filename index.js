75663c9db3409981323fb65aefe6be2e916548e5
AdhamHamed <adhamhamed1029@gmail.com>
2024-09-20 04:33:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,6 @@ const server = http.createServer((req, res) => {
 	}
 });
 
-server.listen(8000, "localhost", () => {
+server.listen(8000, () => {
 	console.log("Listening to requests on port 8000");
 });
